0039ff86d6e91ad9a8c3697d62bf5f540b4475ee
sremedios <sam.remedios@gmail.com>
2017-02-04 11:00:28 -0600

    started calcBestLot.cpp

---
+++ calcBestLot.cpp
@@ -0,0 +1,67 @@
+#include <fstream>
+#include <string>
+#include <map>
+#include <iostream>
+
+#define NUMBER_OF_BUILDINGS 30
+#define NUMBER_OF_LOTS 30
+#define NUMBER_OF_DAYS 5
+#define NUMBER_OF_HOURS 24
+#define DAYS_COL 8
+#define TIME_COL 9
+#define ACT_COL 11
+#define XLACT_COL 14
+
+using std::string;
+using std::map;
+using std::ifstream;
+using std::ofstream;
+using std::cout;
+using std::endl;
+
+struct Building{
+    string name;
+    int numStudents = 0;
+    int timeBlocks[NUMBER_OF_HOURS][NUMBER_OF_DAYS];
+};
+
+struct Lot{
+    string name;
+    int desirability = 0;
+    map<string,int> distFromBuilding;
+};
+
+
+void readFile(string filename);
+
+int main(){
+    Building buildingList[NUMBER_OF_BUILDINGS];
+    Lot lotList[NUMBER_OF_LOTS];
+    string s = "./webscraper/course_data/AAS/testclass.csv"
+
+    return 0;
+}
+
+void readFile(string filename, Building buildinglist)
+/* reads CSV file and holds data in structs */
+{
+    
+    ifstream file(filename);
+    string value;
+    
+    for (int j = 0; j < 57; ++j){
+        getline(file,value,',');
+        //cout << value << endl;
+    }
+    getline(file,value); /* clear last one */
+
+    int i = 0; 
+    while(file.good()){
+        getline(file,value,',');
+        if (i == DAYS_COL || i == TIME_COL || i == ACT_COL || i == XLACT_COL)
+            cout << string(value) << endl;
+        ++i;
+        if (i > 18)
+            i = 0;
+    }
+}
