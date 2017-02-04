5675dcede3873209baa1c5026c46bc22770baf49
Ralisis <ralthebaws@gmail.com>
2017-02-04 01:30:23 -0600

    Distances between each lot and each building

---
+++ distances.py
@@ -0,0 +1,51 @@
+def main():
+        infile1 = open('scaledbuildings.dat','r')
+        infile2 = open('scaledparkinglots.dat','r')
+
+        outfile1 = open('buildingToLotDistance.dat','w')
+
+main()
+
+
+def BuildingPositions():
+    
+    #initialize an array for the building positions
+    #first index is building (determined alphabetically, ranges from 0-29)
+    #second index holds x and y values (ranges from 0-1)
+    buildPos =  [[]]
+
+    #scan building location data and put the data in an array
+    for i in range(0,30):
+            str1 = infile.read()
+            for j in range(0,2):
+                buildPos[i][j] = int(infile.read())
+            int1 = int(infile.read())
+                
+    return buildPos
+
+def LotPositions():
+    #initializes an array for the parking lot positions
+    #first index is parking lots (determined alphabetically, ranges from 0-29)
+    #second index is x,y coordinates (ranges from 0-1)
+    lotPos = [[]]
+        
+    #scan parking lot location data and put the data in an array
+    for i in range(0,30):
+        str1 = infile2.read()
+        for j in range(0,2):
+            lotPos[i][j] = int(infile.read())
+    
+    return lotPos
+
+def Distances():
+    #create an array to store the distances between each parking lot and each building
+    #first index indicates the parking lot
+    #second index indicates the building
+    distances = [[]]
+
+    #for loop that adds the distances between each parking lot and each building
+    for i in range(0,30):
+        for j in range(0,2):
+            
+
+    return distances
