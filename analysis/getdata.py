3f2b3e75d7c8f351b65990cfae8e9002b01fd4fd
Ben Burton <benjaminburton@gmail.com>
2017-02-04 10:48:33 -0600

    fixed get_array function

--- analysis/getdata.py
+++ analysis/getdata.py
@@ -1,20 +1,19 @@
 def main():
-    buildings = getarray("../data/parkinglots.dat")
-    buildings = getarray("../data/buildings.dat")
+    parking_lots = get_array("../data/parkinglots.dat")
+    buildings = get_array("../data/buildings.dat")
     print(buildings)
+    print(parking_lots)
 
-
-def getarray(filename):
+def get_array(filename):
     print(filename)
     data_array  = []
     in_file = open(filename,"r")
     max = len(in_file.readline().split())
-    print(max)
     for line in in_file:
         data_array.append(line.split())
-        for i in range(1,len(line)):
+        for i in range(1,max):
             data_array[-1][i] = int(data_array[-1][i])
     in_file.close()
-    return array
+    return data_array
 
 main()
