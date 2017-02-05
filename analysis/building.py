a5146e65b4327393ef12e1c01861752233ca6f39
FreyAlecJ <alec.j.frey@gmail.com>
2017-02-05 01:36:19 -0600

    Fixed loop count to acount for extra buildings

--- analysis/building.py
+++ analysis/building.py
@@ -3,15 +3,15 @@ import json
 myIn = open("./AlecsBane.txt",'r')
 
 buildings_dict = {}
-for x in range(40):
-    building = myIn.readline().rstrip()
+for x in range(53):
+    building = myIn.readline().rstrip('\n')
     density_of_day = []
     for y in range(5):
         density_of_hour = []
         for z in range(24):
-            density_of_hour.append(myIn.readline().rstrip())
+            density_of_hour.append(myIn.readline().rstrip('\n'))
 	density_of_day.append(density_of_hour)
-    buildings_dict.update({building : density_of_day})
+    buildings_dict[building] = density_of_day
 
 
 myIn.close()
