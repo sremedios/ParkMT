ddd26447291d6cc15c7bf58585cb8cf38e7ba12b
Ben Burton <benjaminburton@gmail.com>
2017-02-05 03:07:40 -0600

    finished, only need to add population function

--- analysis/attraction/model.py
+++ analysis/attraction/model.py
@@ -2,6 +2,12 @@
 # This program will determine the attraction value for a parking lot on
 # MTSU campus based on its location, the population of nearby buildings,
 # and the amount of spots in the parking spot.
+
+import operator
+
+#the no no variables
+parking_lots = []
+buildings = []
  
 def main():
     # Open lot and building position data files and convert to arrays
@@ -11,17 +17,24 @@ def main():
     parking_lots=get_array("../../data/parkinglots.dat")
     buildings =  get_array("../../data/buildings.dat")
 
-    print(parking_lots)
-
 #    day, time, destination = get_user_input()
-    day, time, destination = 15, 15, 15
+    day, time, destination = 1, 15, 10
+
     # get attractiveness of each lot based on input
-    attractions = []
+    attractions = {}
     for lot in parking_lots:
-        attractions.append(attractiveness(lot, destination, time, day))
+        building_name = lot[0]
+        attractions[building_name] = attractiveness(lot, buildings[destination], time, day)
+
+    # sort by attraction
+    sorted_attractions = sorted(attractions.items(), key=lambda kv: kv[1], reverse=True)
+#    print(sorted_attractions)
+
     # ouput building ranked by attraction
-    for attraction in attractions:
-        print(attraction)
+    weekday = "MTWRF"
+    print("On",weekday[day],"at",time,"going to",buildings[destination][0],"these lots have these attractions:")
+    for i in range(len(sorted_attractions)):
+        print(format(i,'2'),format(sorted_attractions[i][0],'15'),sorted_attractions[i][1])
 
 # This function takes the input files and turn them in to arrays.
 # Each element corresponds to one building or parking lot.
@@ -45,12 +58,12 @@ def get_array(filename):
 # distance = sqrt([x1-x2]^2+[y1-y2]^2)
 # Inputs should be building[i] and/or parking_lot[i]
 def get_distance(place1, place2):
-    print(place1, place2)
     x1 = place1[1]
     x2 = place2[1]
     y1 = place1[2]
     y2 = place2[2]
-    return (((x1-x2)(x1-x2)) + ((y1-y2)(y1-y2)))**0.5
+    distance = ((x1-x2)**2 + (y1-y2)**2)**0.5
+    return distance
 
 # Ask user for day, time, and building. building should be number.
 def get_user_input():
@@ -102,26 +115,26 @@ def attractiveness(lot,destination,time,day):
 # domain is 0 < y < 1
 def distance_preference(lot, destination):
     A = 4.0  # This is a parameter we can play with
-    distance = get_distance(lot,destination)
+    distance = get_distance(lot, destination)
     preference = 1/(A*distance/5280.0 + 1.0)
     return preference
 
 # This function returns a calculated value for amount of spots available.
 # Value returned can be negative. Might change and make it return 0 if negative
-def available_parking(lot, time):
+def available_parking(lot, time, day):
     capacity = lot[2]
     # sum up all that may be parked
     total_parked = 0
     for building in buildings:
-        total_parked += parked(lot,building,time)
+        total_parked += parked(lot, building, time, day)
 
     available = capacity - total_parked
     return available
 
 # This function will predict how many people from a building will be parked in a particular lot
-def parked(lot,building,time):
+def parked(lot, building, time, day):
     distance = get_distance(lot, building)
-    pop = population(building, time)
+    pop = population(building, time, day)
     # Sum up total distance of all paths from every parking lot to a building.
     total_distance = 0
     for lot in parking_lots:
@@ -133,7 +146,7 @@ def parked(lot,building,time):
 # This function will return the theoretical population for a given time.
 # This function is probably going to cause the most error in our results.
 def population(building, time, day):
-    return 50
+    return 500
 
 
 
